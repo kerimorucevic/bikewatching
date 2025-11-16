4605b1df5a14c64ccab6a7fac14ed4ee91ff44f6
kerimorucevic <166185519+kerimorucevic@users.noreply.github.com>
2025-11-15 18:56:50 -0800

    the one

--- map.js
+++ map.js
@@ -68,7 +68,7 @@ map.on('load', async () => {
 
     let jsonData;
     try {
-          const jsonurl = INPUT_BLUEBIKES_CSV_URL;
+          const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
       
           // Await JSON fetch
           const jsonData = await d3.json(jsonurl);
